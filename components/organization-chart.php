ca07688e30d4d648319bf04439b6e78b6381b9ec
Pawit Limpasuthum <58322199+BotGem@users.noreply.github.com>
2026-05-27 17:35:58 +0900

    Organizational role block implementation
    Added block that contains information about each role in the organization

---
+++ components/organization-chart.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="component-styles/organization-chart-style.css">
+<header class="nav-bar">
+    <section class="organization-chart">
+        <div class="organization-block">
+            <div  id="coloring">
+                <div class="icon"></div> 
+            </div>
+            <div id="text-content">
+                <h2>Org Advisor</h2>
+                <p>Advisors of current Exec Team</p>
+            </div>
+        </div>
+    </section>
+</header>
