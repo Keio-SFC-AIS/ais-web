de1f56f61f59ac9eda7c58a33de1c463ae500083
Pawit Limpasuthum <58322199+BotGem@users.noreply.github.com>
2026-06-11 13:14:46 +0900

    Added organizational chart and AIS goals

---
+++ components/org-goals.php
@@ -0,0 +1,37 @@
+<link rel="stylesheet" href="../components/component-styles/org-goals-style.css">
+
+<section class="organization-goals">
+    <div class="header-text">
+        <h1>
+            About <span id="ais-text">AIS</span>
+        </h1>
+        <h3>Our Goals</h3>
+        <p>Bridging the gap between GIGA & Non-GIGA students through <span>A.I.S</span></p>
+    </div>
+
+
+    <div class="main-goals">
+        <div class="goal">
+            <div class="acronym">
+                <h2>A.</h2>
+                <img src="../img-resources/interaction.jpg" alt="student interaction">
+            </div>
+            
+            <p>Advocating student engagement and connection.</p>
+        </div>
+        <div class="goal">
+            <div class="acronym">
+                <h2>I.</h2>
+                <img src="../img-resources/international.jpg" alt="student interaction">
+            </div>
+            <p>Integrating international students into a broader community.</p>
+        </div>
+        <div class="goal">
+            <div class="acronym">
+                <h2>S.</h2>
+                <img src="../img-resources/collaboration.jpg" alt="student interaction">
+            </div>
+            <p>Supporting student voices and shared experiences</p>
+        </div>
+    </div>
+</section>
