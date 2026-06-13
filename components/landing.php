acb86115110e10ee277a519187efa35af121a1c0
Pawit Limpasuthum <58322199+BotGem@users.noreply.github.com>
2026-06-13 15:30:38 +0900

    Implemented landing page and index page merge

---
+++ components/landing.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="../components/component-styles/landing-styles.css">
+
+<section class="landing page-section">
+    <div class="header-text">
+        <h1>AIS</h1>
+        <p>"Connecting the bridge between international and Japanese students"</p>
+        <a href="#organization-goals">Explore more</a>
+    </div>
+    
+    <img class="campus" src="../img-resources/sfc.png" alt="sfc campus">
+</section>
