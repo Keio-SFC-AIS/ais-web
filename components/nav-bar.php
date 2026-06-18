17fe310363e9d43d26d386af9e1d88be39460aa4
Tian Yi <tianyi@tianyibrad.com>
2026-06-18 15:49:02 +0900

    Opt: Support responsive design for index page

--- components/nav-bar.php
+++ components/nav-bar.php
@@ -1,11 +1,14 @@
 <header class="nav-bar">
     <nav>
-        <img id="ais-logo" src="img-resources/AIS-logo.png" alt="">
+        <img id="ais-logo" src="img-resources/AIS-logo.png" alt="AIS logo">
+        <button class="nav-toggle" aria-label="Toggle navigation" aria-expanded="false" type="button">
+            <span class="nav-toggle-icon"></span>
+        </button>
         <ul class="nav-list">
             <li><a href="/">Home</a></li>
             <li><a href="/about">About Us</a></li>
             <li><a href="/hub/">AIS Hub</a></li>
+            <li><a href="contact.php" class="contact-us-btn">Contact AIS</a></li>
         </ul>
-        <a class="contact-us-btn" href="contact.php">Contact AIS</a>
     </nav>
 </header>
