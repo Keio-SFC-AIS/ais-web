c382868d1ec75be8629b19ebfe937e3107383a2a
Tian Yi <tianyi@tianyibrad.com>
2026-06-25 18:16:23 +0900

    Add: about page; pjax-like nav; standard heading design; /about/ and /contact/

--- includes/header.php
+++ includes/header.php
@@ -6,15 +6,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AIS</title>
-    <link rel="stylesheet" href="full-pages/full-page-styles/index-styles.css">
-    <link rel="stylesheet" href="components/component-styles/nav-bar-style.css">
-    <link rel="stylesheet" href="components/component-styles/landing-styles.css">
-    <link rel="stylesheet" href="components/component-styles/org-goals-style.css">
-    <link rel="stylesheet" href="components/component-styles/joinus-styles.css">
-    <link rel="stylesheet" href="components/component-styles/organization-chart-style.css">
-    <link rel="stylesheet" href="components/component-styles/about-us-styles.css">
-    <link rel="stylesheet" href="components/component-styles/contact-us-styles.css">
-    <link rel="icon" href="img-resources/AIS-logo.png" type="image/png">
-    <script src="script.js" defer></script>
+    <link rel="stylesheet" href="/full-pages/full-page-styles/index-styles.css">
+    <link rel="stylesheet" href="/components/component-styles/nav-bar-style.css">
+    <link rel="stylesheet" href="/components/component-styles/landing-styles.css">
+    <link rel="stylesheet" href="/components/component-styles/org-goals-style.css">
+    <link rel="stylesheet" href="/components/component-styles/joinus-styles.css">
+    <link rel="stylesheet" href="/components/component-styles/organization-chart-style.css">
+    <link rel="stylesheet" href="/components/component-styles/about-us-styles.css">
+    <link rel="stylesheet" href="/components/component-styles/about-page-styles.css">
+    <link rel="stylesheet" href="/components/component-styles/contact-us-styles.css">
+    <link rel="icon" href="/img-resources/AIS-logo.png" type="image/png">
+    <script src="/script.js" defer></script>
 </head>
 <body>
