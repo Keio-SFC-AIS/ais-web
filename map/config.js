ba107a803907b43c7bf842f39dde623e45336fcd
Keio-SFC-AIS <action@github.com>
2026-07-24 05:07:58 +0000

    Update from https://github.com/BradleyBao/NetSFC/commit/6e90dafe593bcec4e9b48a13a3d717d69bc36d1c

--- map/config.js
+++ map/config.js
@@ -1,3 +1,11 @@
+const IS_LOCAL = [
+    'localhost',
+    '127.0.0.1',
+    '::1'
+].includes(window.location.hostname);
+
 window.ENV = {
-    API_HOST: "http://localhost:8080" // localhost
+    API_HOST: IS_LOCAL
+        ? "http://localhost:8080"
+        : "https://netsfc-api.tianyibrad.com"
 };
